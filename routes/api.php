c238dec3c29789546329493f88768a91ec689f94
Fabio La Farcioli <fabio.lafarcioli@parkingmycar.it>
2023-09-28 14:53:00 +0200

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\Auth\LoginController;
+use App\Http\Controllers\Api\v1\InvoiceController;
+use App\Http\Controllers\Api\v1\SaleController;
+use App\Http\Controllers\Api\v1\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,71 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::name('auth.')
+    ->prefix('auth')
+    ->middleware(['api'])
+    ->group(
+        function () {
+            Route::post('/login', LoginController::class)
+                ->middleware('guest')
+                ->name('login');
+
+        });
+
+Route::group(
+    ['middleware' => [
+        'auth:api',
+    ]],
+    function () {
+
+        Route::prefix('services')
+            ->name('services.')
+            ->group(
+                function () {
+
+                    Route::get('/', [ServiceController::class, 'index'])
+                        ->name('index');
+
+                    Route::get('/show/{service}', [ServiceController::class, 'show'])
+                        ->name('show');
+
+                    Route::post('/store', [ServiceController::class, 'store'])
+                        ->name('store');
+
+                    Route::put('/update/{service}', [ServiceController::class, 'update'])
+                        ->name('update');
+
+                    Route::patch('/status/{service}', [ServiceController::class, 'updateStatus'])
+                        ->name('update_status');
+
+                    Route::delete('/delete/{service}', [ServiceController::class, 'delete'])
+                        ->name('delete');
+
+                });
+
+        Route::prefix('sales')
+            ->name('sales.')
+            ->group(
+                function () {
+
+                    Route::get('/', [SaleController::class, 'index'])
+                        ->name('index');
+
+                }
+            );
+
+        Route::prefix('invoices')
+            ->name('invoices.')
+            ->group(
+                function () {
+
+                    Route::get('/', [InvoiceController::class, 'index'])
+                        ->name('index');
+
+                    Route::post('/', [InvoiceController::class, 'store'])
+                        ->name('store');
+                }
+            );
+
+    }
+);
